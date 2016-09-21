182ffc061d06abbc29022a4e3e6620a28baa1ed2
Azat Khalilov <billlater@yandex.ru>
2016-09-21 13:12:52 +0300

    define universal module definition

--- src/azQueryBuilder.js
+++ src/azQueryBuilder.js
@@ -1,6 +1,15 @@
-(function (func) {
-    func()
-})(function () {
+(function (root,factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define(factory);
+    } else if (typeof exports === 'object') {
+        // Node, CommonJS-like
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory();
+    }
+})(this,function () {
     angular.module('azQueryBuilderModule', [])
         .factory('azQueryBuilderCore', azQueryBuilderCore)
         .directive('azQueryBuilder', azQueryBuilder)
